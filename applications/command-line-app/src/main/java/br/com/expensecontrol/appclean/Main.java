83be68b4efff992ed54e8f2bfe998ee57921386a
brunobarreira <brunobarreirasilva@gmail.com>
2021-09-13 19:48:33 -0300

    feat: add command line app module

---
+++ applications/command-line-app/src/main/java/br/com/expensecontrol/appclean/Main.java
@@ -0,0 +1,31 @@
+package br.com.expensecontrol.appclean;
+
+import br.com.expensecontrol.appclean.config.ManualConfig;
+import com.expensecontrol.appclean.ExpenseController;
+import com.expensecontrol.appclean.presenter.ExpensePresenter;
+
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("-----------------------------------");
+                System.out.println("----WELCOME, TO THE EXPENSE APP----");
+                System.out.println("-----------------------------------");
+
+                System.out.println("ARGUMENTS WERE PROVIDED VIA IDE, RECORDING EXPENSE ON MEMORY...");
+                var config = new ManualConfig();
+                var createExpense = config.createExpense();
+                var controller = new ExpenseController(createExpense);
+
+                var expenseModel = new ExpensePresenter();
+                expenseModel.setExp_price(args[0]);
+                expenseModel.setExp_description(args[1]);
+                expenseModel.setExp_category(args[2]);
+                expenseModel.setExp_date(args[3]);
+
+                final var expenseCreated = controller.createExpense(expenseModel);
+
+                System.out.println("[EXPENSE CREATED]");
+                System.out.println(expenseCreated);
+
+                System.out.println("DONE, BYE!");
+        }
+}
---
+++ applications/command-line-app/src/main/java/br/com/expensecontrol/appclean/config/ManualConfig.java
@@ -0,0 +1,14 @@
+package br.com.expensecontrol.appclean.config;
+
+import com.expensecontrol.appclean.CreateExpense;
+import com.expensecontrol.appclean.db.InMemoryExpenseRepository;
+import com.expensecontrol.appclean.port.ExpenseRepository;
+
+public class ManualConfig {
+
+    private final ExpenseRepository database = new InMemoryExpenseRepository();
+
+    public CreateExpense createExpense() {
+        return new CreateExpense(database);
+    }
+}
